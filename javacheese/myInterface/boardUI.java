c7a2ba4fc5a39f9e428f1ab9a28c61cb0743e580
Jue wang <32614178+Wangjue74@users.noreply.github.com>
2019-01-03 19:31:11 +0800

    Add files via upload

---
+++ javacheese/myInterface/boardUI.java
@@ -0,0 +1,11 @@
+package myInterface;
+//swing
+import javax.swing.*;
+import java.awt.*; 
+import java.awt.Color;
+import java.awt.Container;
+public class boardUI extends JPanel{
+	public boardUI(){
+		
+	}
+}
